b81a6728c3f6f606dc55d0192541c40d5676289e
kaylavera99 <kaylavd99@gmail.com>
2024-06-29 21:20:25 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'we-eat',
+  webDir: 'dist'
+};
+
+export default config;
